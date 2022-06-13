503e615eff4e018ea08ff19730c2fc27a5639fb0
Zhiero-juro <qzdgremio@tip.edu.ph>
2022-06-14 01:07:13 +0800

    Adding a new SignUp Screen

--- App.js
+++ App.js
@@ -6,6 +6,7 @@ import GetStartedScreen from './App/Screens/GetStarted'
 import GetNotifiedScreen from './App/Screens/GetNotified'
 import AreYouAReaderScreen from './App/Screens/AreYouAReader'
 import LoginScreen from './App/Screens/Login'
+import SignUpScreen from './App/Screens/SignUp'
 import { useFonts } from 'expo-font'
 
 const NavigationStack = createNativeStackNavigator()
@@ -44,6 +45,9 @@ export default function App() {
         </NavigationStack.Screen>
         <NavigationStack.Screen name="Login" options={{ headerShown: false }}>
           {(props) => <LoginScreen {...props} />}
+        </NavigationStack.Screen>
+        <NavigationStack.Screen name="SignUp" options={{ headerShown: false }}>
+          {(props) => <SignUpScreen {...props} />}
         </NavigationStack.Screen>
       </NavigationStack.Navigator>
     </NavigationContainer>
---
+++ App/Screens/SignUp.js
@@ -0,0 +1,274 @@
+import {
+    TouchableOpacity,
+    View,
+    ImageBackground,
+    StyleSheet,
+    Text,
+    Image,
+    TextInput,
+  } from 'react-native'
+  import { topDesign, logo } from '../../images'
+  import { styles } from '../../styles'
+  import AsyncStorage from '@react-native-async-storage/async-storage'
+  import Zocial from 'react-native-vector-icons/Zocial'
+  import Fontisto from 'react-native-vector-icons/Fontisto'
+  import { Checkbox } from '../Components/ButtonComponents'
+  import { Hr } from '../Components/LineComponent'
+  const SignUp = ({ navigation }) => {
+    return (
+      <View style={{ ...styles.container, backgroundColor: '#D6DBE8', flex: 1 }}>
+        <Image
+          source={topDesign}
+          style={{
+            height: '50%',
+            width: '100%',
+            position: 'absolute',
+            borderBottomLeftRadius: 18,
+            borderBottomRightRadius: 18,
+          }}
+        />
+  
+        <View
+          style={{
+            flex: 1,
+            justifyContent: 'center',
+  
+            paddingHorizontal: 10,
+          }}
+        >
+          <View style={styles.imageContainer}>
+            <Image
+              source={logo}
+              style={{ ...styles.logo, height: 150, width: 150 }}
+            />
+          </View>
+          <View style={{ marginBottom: 40 }}>
+            <Text
+              style={{
+                fontWeight: 'bold',
+                marginLeft: 10,
+                color: '#507CC0',
+                fontSize: 25,
+              }}
+            >
+              Sign Up
+            </Text>
+            <Text
+              style={{
+                fontWeight: 'bold',
+                marginLeft: 10,
+                color: '#721717',
+                fontSize: 15,
+              }}
+            >
+              Please fill the details and create an account.
+            </Text>
+          </View>
+          <View style={{ alignItems: 'center' }}>
+            <View style={styles.card}>
+            <View style={styles.txtInput}>
+                <Zocial
+                  name="persona"
+                  size={22}
+                  style={{ marginRight: 15 }}
+                  color={'grey'}
+                />
+                <TextInput
+                  placeholder="Name"
+                  placeholderTextColor={'grey'}
+                  style={{
+                    color: 'black',
+                    width: '100%',
+                    fontSize: 18,
+                    paddingEnd: 18,
+                    top: -2,
+                  }}
+                  editable={true}
+                  onChangeText={(v) => console.log(v)}
+                />
+              </View>
+              <View style={styles.txtInput}>
+                <Zocial
+                  name="persona"
+                  size={22}
+                  style={{ marginRight: 15 }}
+                  color={'grey'}
+                />
+                <TextInput
+                  placeholder="Username"
+                  placeholderTextColor={'grey'}
+                  style={{
+                    color: 'black',
+                    width: '100%',
+                    fontSize: 18,
+                    paddingEnd: 18,
+                    top: -2,
+                  }}
+                  editable={true}
+                  onChangeText={(v) => console.log(v)}
+                />
+              </View>
+              <View style={styles.txtInput}>
+                <Zocial
+                  name="email"
+                  size={22}
+                  style={{ marginRight: 15 }}
+                  color={'grey'}
+                />
+                <TextInput
+                  placeholder="Email"
+                  placeholderTextColor={'grey'}
+                  style={{
+                    color: 'black',
+                    width: '100%',
+                    fontSize: 18,
+                    paddingEnd: 18,
+                    top: -2,
+                  }}
+                  editable={true}
+                  onChangeText={(v) => console.log(v)}
+                />
+              </View>
+              <View style={styles.txtInput}>
+                <Fontisto
+                  name="unlocked"
+                  size={22}
+                  style={{ marginRight: 15 }}
+                  color={'grey'}
+                />
+                <TextInput
+                  placeholder="Password"
+                  placeholderTextColor={'grey'}
+                  style={{
+                    color: 'black',
+                    width: '100%',
+                    fontSize: 18,
+                    paddingEnd: 18,
+                    top: -2,
+                  }}
+                  editable={true}
+                  onChangeText={(v) => console.log(v)}
+                />
+              </View>
+              <View
+                style={{
+                  padding: 10,
+                  flexDirection: 'row',
+                  paddingVertical: 20,
+                  flex: 1,
+                  width: "100%"
+                }}
+              >
+                
+                <Checkbox onPress={(v) => console.log(v)} />
+             
+                <View style={{flex: 1}}>
+                <Text style={{ marginLeft: 8, color: '#ABAFBA', flex: 1 }}>
+                  By signing up you accept our 
+                        <Text style={{marginLeft: 5, color: 'blue', marginRight: 5}}>Terms & Conditions</Text>
+                              and the 
+                              <Text style={{marginLeft: 5, color: 'blue', marginRight: 5}}>Policy</Text>
+                </Text>
+                </View>
+               
+              
+             
+                
+              </View>
+              <TouchableOpacity style={styles.loginButton}>
+                <Text
+                  style={{
+                    width: '100%',
+                    textAlign: 'center',
+                    fontWeight: 'bold',
+                    color: 'white',
+                    fontSize: 16,
+                  }}
+                >
+                  Sign Up
+                </Text>
+              </TouchableOpacity>
+            </View>
+            <View
+              style={{
+                width: '100%',
+                flexDirection: 'row',
+                flex: 1,
+                marginTop: 20,
+                marginHorizontal: 5,
+                height: 18,
+                marginVertical: 18,
+              }}
+            >
+              <Hr style={{ flex: 1, height: 8, backgroundColor: '#FF8DAD' }} />
+              <View style={{ flex: 1, height: 18, top: -7 }}>
+                <Text style={{ textAlign: 'center' }}>Or connect using</Text>
+              </View>
+              <Hr style={{ flex: 1, height: 8, backgroundColor: '#FF8DAD' }} />
+            </View>
+            <View style={styles.card}>
+              <View style={{ flexDirection: 'row', marginBottom: 10 }}>
+                <TouchableOpacity
+                  style={[
+                    styles.loginButton,
+                    { flex: 1, marginHorizontal: 5, backgroundColor: '#004871' },
+                  ]}
+                >
+                  <Text
+                    style={{
+                      width: '100%',
+                      textAlign: 'center',
+                      fontWeight: 'bold',
+                      color: 'white',
+                      fontSize: 16,
+                    }}
+                  >
+                    Facebook
+                  </Text>
+                </TouchableOpacity>
+                <TouchableOpacity
+                  style={[
+                    styles.loginButton,
+                    { flex: 1, marginHorizontal: 5, backgroundColor: '#31B3FD' },
+                  ]}
+                >
+                  <Text
+                    style={{
+                      width: '100%',
+                      textAlign: 'center',
+                      fontWeight: 'bold',
+                      color: 'white',
+                      fontSize: 16,
+                    }}
+                  >
+                    Twitter
+                  </Text>
+                </TouchableOpacity>
+                <TouchableOpacity
+                  style={[
+                    styles.loginButton,
+                    { flex: 1, marginHorizontal: 5, backgroundColor: '#D22F25' },
+                  ]}
+                >
+                  <Text
+                    style={{
+                      width: '100%',
+                      textAlign: 'center',
+                      fontWeight: 'bold',
+                      color: 'white',
+                      fontSize: 16,
+                    }}
+                  >
+                    Google
+                  </Text>
+                </TouchableOpacity>
+              </View>
+            </View>
+          </View>
+        </View>
+      </View>
+    )
+  }
+  
+  export default SignUp
+  
